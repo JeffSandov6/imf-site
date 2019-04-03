27ce44386ab79dbb94f4982b39d8d1e96e00db77
Jeffry Sandoval <jeffrysandoval24@gmail.com>
2019-04-02 20:15:46 -0500

    nav bar is perfected for the most part, just missing some images

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 // import { Navbar } from "react-bootstrap";
 import Routes from './Routes';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+// import './App.css';
+import NavigationBar from './components/NavigationBar/NavigationBar';
+import Home from './pages/Home';
 
 // class App extends React.Component {
 //     render() {
@@ -23,30 +25,44 @@ import './App.css';
 //       }
 // }
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <Router>
-        {/* everything in div is the nav bar stuff */}
+      <div className="App">
+        <NavigationBar />
         <div>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <ul className="navbar-nav mr-auto">
-              <li>
-                <Link to={'/'} className="nav-link">
-                  {' '}
-                  Home{' '}
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          {/* <hr /> */}
-          {/* Routes is our from our Routes.js file */}
-          <Routes />
-          <hr />
+          <Router>
+            <Home />
+          </Router>
         </div>
-      </Router>
+      </div>
     );
   }
 }
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <Router>
+//         {/* everything in div is the nav bar stuff */}
+//         <div>
+//           <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//             <ul className="navbar-nav mr-auto">
+//               <li>
+//                 <Link to={'/'} className="nav-link">
+//                   Home
+//                 </Link>
+//               </li>
+//             </ul>
+//           </nav>
+//           <hr />
+//           {/* Routes is our from our Routes.js file */}
+//           <Routes />
+//           <hr />
+//         </div>
+//       </Router>
+//     );
+//   }
+// }
 
 export default App;
