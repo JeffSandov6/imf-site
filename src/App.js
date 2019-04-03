48aea4d8e8b69998f0c2c4a91d6369ad8e9991a8
Jeffry Sandoval <jeffrysandoval24@gmail.com>
2019-04-02 20:16:50 -0500

    nav bar is perfected for the most part, just missing some images

--- src/App.js
+++ src/App.js
@@ -7,24 +7,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar/NavigationBar';
 import Home from './pages/Home';
 
-// class App extends React.Component {
-//     render() {
-//         return (
-//           <div className="App container">
-//             <Navbar fluid collapseOnSelect>
-//               <Navbar.Header>
-//                 <Navbar.Brand>
-//                   <Link to="/">Innovation Metal Roofing</Link>
-//                 </Navbar.Brand>
-//                 <Navbar.Toggle />
-//               </Navbar.Header>
-//             </Navbar>
-//             <Routes />
-//           </div>
-//         );
-//       }
-// }
-
 class App extends Component {
   render() {
     return (
@@ -39,30 +21,5 @@ class App extends Component {
     );
   }
 }
-
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <Router>
-//         {/* everything in div is the nav bar stuff */}
-//         <div>
-//           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//             <ul className="navbar-nav mr-auto">
-//               <li>
-//                 <Link to={'/'} className="nav-link">
-//                   Home
-//                 </Link>
-//               </li>
-//             </ul>
-//           </nav>
-//           <hr />
-//           {/* Routes is our from our Routes.js file */}
-//           <Routes />
-//           <hr />
-//         </div>
-//       </Router>
-//     );
-//   }
-// }
 
 export default App;
