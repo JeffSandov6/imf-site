b264d997bb9f6a4ecd7e3d5215bd9f058cd10b5a
Jeffry Sandoval <jeffrysandoval24@gmail.com>
2019-04-25 16:55:27 -0500

    adding some extra stuff

--- src/components/Home/HomeLander/HomeLander.js
+++ src/components/Home/HomeLander/HomeLander.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import './HomeLander.css';
+import RotatingImage from '../RotatingImage/RotatingImage';
 
 class HomeLander extends Component {
   render() {
-    return <div className="HomeLander" />;
+    return (
+      <div className="HomeLander" style={{ backgroundColor: '#D3D3D3' }}>
+        <div className="row">
+          <div className="col-sm-12 col-md-3">
+            <div className="logo">
+              <img
+                src={require('./flogo_RGB_HEX-512.png')}
+                alt="missingpic"
+                style={{ width: '100%' }}
+              />
+            </div>
+          </div>
+
+          <div className="col-sm-12 col-md-9">
+            <RotatingImage />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
