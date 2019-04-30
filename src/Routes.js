1adf446d862e646c9e9f8a87204aa466df562e9a
Jeffry Sandoval <jeffrysandoval24@gmail.com>
2019-04-30 18:40:33 -0500

    added the route path to metal building. small change, not worth looking at

--- src/Routes.js
+++ src/Routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import MetalRoofing from './pages/MetalRoofing/MetalRoofing';
+import MetalBuilding from './pages/MetalBuilding/MetalBuilding';
 
 export default () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/metalroofing" component={MetalRoofing} />
+    <Route exact path="/metalbuilding" component={MetalBuilding} />
   </Switch>
 );
